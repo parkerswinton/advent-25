0e19206f77e4a3f813a54f34b59596de91cd9935
Parker Swinton <parkerdswinton@gmail.com>
2025-12-01 16:05:04 -0800

    feat: generate template + run days from root

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import sys
+import os
+import subprocess
+
+default_text = """#!/usr/bin/env python3
+
+def main():
+    print("DEFAULT")
+
+
+if __name__ == \"__main__\":
+    main()
+"""
+
+
+def generate(day):
+    if os.path.exists(day):
+        sys.exit(f"Error: Folder '{day}' already exists.")
+
+    os.mkdir(day)
+
+    with open(os.path.join(day, "main.py"), "w") as f:
+        f.write(default_text)
+
+
+def run(day):
+    file_path = os.path.join(day, "main.py")
+    if not os.path.isfile(file_path):
+        sys.exit(f"Error: Folder '{day}' does not exist.")
+
+    subprocess.run(["python3", file_path])
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python3 main.py <mode> <day_number>")
+        print("Modes: generate (-g), run (-r)")
+        sys.exit()
+
+    mode = sys.argv[1].lower()
+    day = sys.argv[2]
+
+    match mode:
+        case "generate" | "gen" | "g" | "-g":
+            generate(day)
+        case "run" | "r" | "-r":
+            run(day)
+        case _:
+            sys.exit("Error: Unknown mode.")
+
+
+if __name__ == "__main__":
+    main()
